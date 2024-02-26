e7d96a8594c8094a00d0462524fa1d844870b195
Shivendra Thakur <shivendrathakur321@outlook.com>
2024-02-26 23:48:30 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let modelBox = document.getElementById("model-box");
+document.onmouseleave = function() {
+  modelBox.style.display = "block";
+}
+
+function closeModel() {
+  modelBox.style.display = "none";
+}
